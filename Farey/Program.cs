05d3dfaa91a095eeda334f232015b9332f56d99f
kbogdev <kbogdev@gmail.com>
2015-12-22 07:59:31 -0800

    First stage. Test: Red

---
+++ Farey/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Farey
+{
+    public class FareyNumber
+    {
+        public readonly uint N;
+        public readonly uint D;
+
+        public FareyNumber(uint n, uint d)
+        {
+            N = n;
+            D = d;
+        }
+
+        public override string ToString()
+        {
+            string result = String.Format("{0}/{1}", N, D);
+            return result;
+        }
+    }
+
+    public class FareyNumbers
+    {
+        public readonly uint Num;
+        public readonly uint Count;
+
+        public readonly HashSet<FareyNumber> Numbers = new HashSet<FareyNumber>();
+
+        public FareyNumbers(uint N)
+        {
+            Num = N;
+            Count = (uint)Numbers.Count;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ FareyUnitTests/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Farey;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FareyUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            FareyNumbers nums = new FareyNumbers(1);
+            Assert.IsTrue(nums.Count == 2);
+
+            string result = "0/1,1/1";
+            Assert.IsTrue(0 == nums.ToString().CompareTo(result));
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            FareyNumbers nums = new FareyNumbers(2);
+            Assert.IsTrue(nums.Count == 3);
+
+            string result = "0/1,1/2,1/1";
+            Assert.IsTrue(0 == nums.ToString().CompareTo(result));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            FareyNumbers nums = new FareyNumbers(5);
+            Assert.IsTrue(nums.Count == 11);
+
+            string result = "0/1,1/5,1/4,1/3,2/5,1/2,3/5,2/3,3/4,4/5,1/1";
+            Assert.IsTrue(0 == nums.ToString().CompareTo(result));
+        }
+    }
+}
